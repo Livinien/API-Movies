4d45f66e47e5bf6843c51e3f27579e5463be76c2
Flavien <livinien@hotmail.fr>
2023-02-07 16:23:49 +0100

    Récupérer les titres des films de l'API

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,7 +17,7 @@ function fetchMovies() {
 
     movies = fetch(`https://api.themoviedb.org/3/search/movie?api_key=8ce9f8f28953d4d7871c62b5ac654555&query=${valueInput}`)
     .then(response => response.json());
-    
+  
     console.log(movies);
   })
 };
@@ -27,12 +27,10 @@ function fetchMovies() {
 
 // RECUPERER LES FILMS LES MIEUX NOTES - PERMETTANT D'AFFICHER QUELQUES FILMS DE PRESENTATION SUR LA PAGE D'ACCUEIL
 
-function getTopRatedMovies() {
-  let movies = fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=8ce9f8f28953d4d7871c62b5ac654555`)
-    .then(response => response.json());
-
-    return movies;
-}
+// function getTopRatedMovies() {
+  
+    
+// };
 
 
 // AFFICHER LES POSTERS DES FILMS
@@ -41,32 +39,44 @@ function moviesApi() {
 console.log("movieApi");
 
 
-  let movies = getTopRatedMovies();
+  // let movies = getTopRatedMovies();
+  // console.log("movies");
+  // console.log(movies);
+
+
+  let movies = fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=8ce9f8f28953d4d7871c62b5ac654555`)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+
+    let movies = data.results;
+
+    for(let i = 0; i < movies.length; i++)  {
+      console.log("i " + i);
+      console.log(movies[i].title);
+      let apiMovies = document.querySelector(".movies");
+      
+      let moviesDiv = document.createElement("div");
+      moviesDiv.setAttribute("class", "movie");
+      apiMovies.appendChild(moviesDiv);
+      
+      
+      let posterMovie = document.createElement("img");
+      posterMovie.setAttribute("class", "posterMovie");
+      posterMovie.src = "https://www.themoviedb.org/movie" + movies[i].poster_path;
+      moviesDiv.appendChild(posterMovie);
+      
+      let title = document.createElement("h1");
+      title.setAttribute("class", "title");
+      title.textContent = movies[i].title;
+      moviesDiv.appendChild(title);
+    }
+    
+  });
+
   console.log(movies);
 
-  for(let i = 0; i < movies.length; i++)  {
-    console.log("i " + i);
-    let apiMovies = document.querySelector(".movies");
-    
-    let movies = document.createElement("div");
-    movies.setAttribute("class", "movie");
-    apiMovies.appendChild(movies);
-    
-    
-    let posterMovie = document.createElement("img");
-    posterMovie.setAttribute("class", "posterMovie");
-    posterMovie.src = movies[i].poster_path;
-    movies.appendChild(posterMovie);
-    
-    let title = document.createElement("h1");
-    title.setAttribute("class", "title");
-    title.textContent = movies[i].title;
-    movies.appendChild(title);
-  }
-  
-  console.log(movies);
-
-}
+};
 
 moviesApi();
   
